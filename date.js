7514466b86c7fe166c1d92166ac8854fb045593d
kiishi14 <aladesuru441@gmail.com>
2023-02-02 10:19:09 +0100

    todolist first commit

---
+++ date.js
@@ -0,0 +1,21 @@
+exports.getDate = function () {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  return today.toLocaleDateString("en-NG", options);
+};
+
+module.exports.getDay = function () {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-NG", options);
+};
