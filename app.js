87506d80a85ba02957e50be490d17ef92cf019fd
kiishi14 <aladesuru441@gmail.com>
2023-02-02 10:32:33 +0100

    added process.env.PORT

--- app.js
+++ app.js
@@ -178,6 +178,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server started on port 3000");
 });
